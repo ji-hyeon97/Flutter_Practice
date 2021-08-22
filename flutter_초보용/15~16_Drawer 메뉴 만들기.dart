b1cd4cbfae4735ff6831062288d04becdee9ea5f
ji-hyeon97 <79920930+ji-hyeon97@users.noreply.github.com>
2021-08-22 13:32:05 +0900

    Update 15~16_Drawer 메뉴 만들기.dart

--- flutter_초보용/15~16_Drawer 메뉴 만들기.dart
+++ flutter_초보용/15~16_Drawer 메뉴 만들기.dart
@@ -1,3 +1,12 @@
+// App bar icon button
+// 1. leading : 아이콘 버튼이나 간단한 위젯을 왼쪽에 배치할 때
+// 2. actions : 복수의 아이콘 버튼 등을 오른쪽에 배치할 때
+// 3. onPressed : 함수의 형태로 일반 버튼이나 아이콘 버튼을 터치했을 때 일어나는 이벤트를 정의하는 곳
+// Drawer menu
+// Drawer -> ListView -> UserAccountDrawerHeader
+// ->currentAccountPicture, accountName, accountEmail, onDetailsPressed, decoration, otherAccountPictures
+// Drawer -> ListView -> ListTile ->Icon, Text, onTap
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,11 +18,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Appbar",
+      title: 'Appbar',
       theme: ThemeData(
-        primarySwatch: Colors.red
+        primarySwatch: Colors.green
       ),
-     home: MyPage(),      
+      home: MyPage(),
     );
   }
 }
@@ -24,104 +33,90 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Appbar icon menu'),
+        title: Text("서지현"),
         centerTitle: true,
         elevation: 0.0,
-        
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.shopping_cart), 
-          onPressed: () { 
-            print("Shopping_cart button is clicked");
+        actions: [ //복수의 아이콘 버튼 등을 오른쪽에 배치할 때
+          IconButton( //IconButton터치시 반응이 있어야 함
+          icon: Icon(Icons.shopping_cart), //쇼핑카트 모양 메뉴
+          onPressed: () {  //onPressed -> void function 아이콘버튼 클릭시 반환값이 없는 함수
+            print('서지현 shoppingcart button is clicked'); //IconButton을 위한 액션
            },
-      ),
-       IconButton(
-          icon: Icon(Icons.search), 
-          onPressed: () { 
-            print("Search button is clicked");
+          ),
+          IconButton( //IconButton터치시 반응이 있어야 함
+          icon: Icon(Icons.search), //돋보기 모양 메뉴
+          onPressed: () {  //onPressed -> void function 아이콘버튼 클릭시 반환값이 없는 함수
+            print('서지현 search button is clicked'); //IconButton을 위한 액션
            },
-      ),
-      ],
+          ),
+        ],
       ), 
-      drawer: Drawer(//사이드 메뉴
-        child:ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/이즈리얼.png'),
+      drawer: Drawer(//앱바에서 leading으로 icon을 만드는 것이 아니라 앱바 끝나는 곳에서 drawer 기능 추가
+      child: ListView(
+        padding: EdgeInsets.zero, //여백 X
+        children: [ //목록들
+          UserAccountsDrawerHeader( //ctrl 위젯클릭후 required 부분 있으면 확인하기, 사용자
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage('assets/이즈리얼.png'),
+              backgroundColor: Colors.white, //보통 투명 배경이미지인 경우 사용
+            ),
+            otherAccountsPictures: [ //다른 사용자(계정)
+              CircleAvatar( //1번 사용자
                 backgroundColor: Colors.white,
+                backgroundImage: AssetImage('assets/야스오.png'),
               ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/야스오.png'),
-                  backgroundColor: Colors.white,
-                ),
-                
-              ],
-              accountName: Text('이즈리얼'),
-              accountEmail: Text('sgh9702@naver.com'),
-              onDetailsPressed: (){
-                print('arrow is clicked');
-              },
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40.0),//곡선율
-                  bottomRight: Radius.circular(40.0)
-                )
-              ),
+              CircleAvatar( //2번 사용자
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('assets/야스오.png'),
+              )
+            ],
+            
+            accountName:  Text("서지현"), //UserAccountsDrawerHeader 위젯을 필수 요소
+            accountEmail: Text("sgh9702@naver.com"),//UserAccountsDrawerHeader 위젯의 필수 요소
+            onDetailsPressed: (){ //화살표기능 부여
+              print("arrow is clicked"); //액션
+            },
+            decoration: BoxDecoration(
+              color: Colors.greenAccent,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20.0), //왼쪽 곡선율 지정
+                bottomRight: Radius.circular(20.0) //오른쪽 곡선율 지정
+              )
             ),
-            ListTile(
-              leading: Icon(Icons.home,
-              color: Colors.grey[850],
-              ),
-              title: Text('Home'),
-              onTap: (){//화면을 클릭한 경우
-                print('Home is clicked');
-              },
-              trailing: Icon(Icons.add),
-              
+          ),
+          ListTile( //ListTile
+            leading: Icon(Icons.home, //집 모양 아이콘 , 왼쪽 시작하는 지점(leading)
+            color: Colors.grey[850], //진하게
             ),
-             ListTile(
-              leading: Icon(Icons.settings,
-              color: Colors.grey[850],
-              ),
-              title: Text('settting'),
-              onTap: (){
-                print('setting is clicked');
-              },
-              trailing: Icon(Icons.add),
-              
+            title: Text('Home'), //제목
+            onTap: (){ //액션을 주어야 함
+              print('Home is clicked');
+            },
+            trailing: Icon(Icons.add), //오른쪽 끝나는 지점(trailing)
+          ),
+           ListTile( //ListTile
+            leading: Icon(Icons.settings, //세팅 모양 아이콘 , 왼쪽 시작하는 지점(leading)
+            color: Colors.grey[850], //진하게
             ),
-             ListTile(
-              leading: Icon(Icons.question_answer,
-              color: Colors.grey[850],
-              ),
-              title: Text('Q&A'),
-              onTap: (){
-                print('Q&A is clicked');
-              },
-              trailing: Icon(Icons.add),
-              
+            title: Text('setting'), //제목
+            onTap: (){ //액션을 주어야 함
+              print('setting is clicked');
+            },
+            trailing: Icon(Icons.add), //오른쪽 끝나는 지점(trailing)
+          ),
+           ListTile( //ListTile
+            leading: Icon(Icons.question_answer, //집 모양 아이콘 , 왼쪽 시작하는 지점(leading)
+            color: Colors.grey[850], //진하게
             ),
-          ],
-        ),
+            title: Text('Q&A'), //제목
+            onTap: (){ //액션을 주어야 함
+              print('Q&A is clicked');
+            },
+            trailing: Icon(Icons.add), //오른쪽 끝나는 지점(trailing)
+          ),
+        ],
+      ),
       ),
     );
   }
 }
-
-/*
-draswer -> Listview -> UserAccountDrawerHeader
-1. otherAccountPictures
-2. currentAccountPicture
-3. accountName
-4. accountEmail
-5. onDetailsPressed
-
-draswer -> Listview -> ListTile
-1. Icon
-2. Text
-3. onTap
-*/
